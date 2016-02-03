72c37c7d3d148eb38282de553fd106fa7a156e18
leeza1222 <leeza1222@gmail.com>
2016-02-03 17:06:27 +0700

    Strart App

---
+++ app/src/main/java/pimpacht/chatchai/weightcheck/WeightCheck.java
@@ -0,0 +1,13 @@
+package pimpacht.chatchai.weightcheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeightCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_check);
+    }
+}
